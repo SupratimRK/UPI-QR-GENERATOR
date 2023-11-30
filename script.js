8b21e410ef27fe935789bc23b9778d7f26f86b76
Supratim Mondal <you@example.com>
2023-11-30 12:06:16 +0530

    added account no and mobile no function updated ui

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 function generateQRCode() {
+    const toggleSwitch = document.getElementById('toggleSwitch');
     const upiId = document.getElementById('upiId').value.trim();
     const amount = document.getElementById('amount').value.trim();
+    const acNumber = document.getElementById('acNumber').value.trim();
+    const ifsc = document.getElementById('ifsc').value.trim();
+    const acAmount = document.getElementById('acAmount').value.trim();
 
-    if (!upiId) {
-        alert('Please enter a valid UPI ID.');
-        return;
+    if (toggleSwitch.checked) {
+        // AC mode selected
+        if (!acNumber || !ifsc) {
+            alert('Please enter AC number and IFSC code.');
+            return;
+        }
+
+        const acId = `${acNumber}@${ifsc}.ifsc.npci`;
+        const qrText = generateUPIQRText(acId, acAmount);
+        displayQRCode(qrText);
+    } else {
+        // VPA mode selected
+        if (!upiId) {
+            alert('Please enter a valid UPI ID.');
+            return;
+        }
+
+        const qrText = generateUPIQRText(upiId, amount);
+        displayQRCode(qrText);
     }
+}
 
-    const qrText = generateUPIQRText(upiId, amount);
-    displayQRCode(qrText);
+function toggleInputsVisibility() {
+    const toggleSwitch = document.getElementById('toggleSwitch');
+    const upiInputs = document.getElementById('upiInputs');
+    const acInputs = document.getElementById('acInputs');
+
+    if (toggleSwitch.checked) {
+        // AC mode selected, hide UPI inputs and show AC inputs
+        upiInputs.style.display = 'none';
+        acInputs.style.display = 'block';
+    } else {
+        // VPA mode selected, hide AC inputs and show UPI inputs
+        upiInputs.style.display = 'block';
+        acInputs.style.display = 'none';
+    }
 }
 
 function generateUPIQRText(upiId, amount) {
@@ -31,8 +64,8 @@ function displayQRCode(qrText) {
 
     const qrcode = new QRCode(resultContainer, {
         text: qrText,
-        width: 360,
-        height: 360,
+        width: 440,
+        height: 440,
         colorDark: "#b1f202", // Set the dark color to #b1f202
         colorLight: "#fff",   // Set the light color to #fff
     });
@@ -40,6 +73,8 @@ function displayQRCode(qrText) {
     resultContainer.style.display = 'block';
 }
 
+// Event listener for toggle switch change
+document.getElementById('toggleSwitch').addEventListener('change', toggleInputsVisibility);
 
-
-
+// Call toggleInputsVisibility on page load to set initial state
+toggleInputsVisibility();
